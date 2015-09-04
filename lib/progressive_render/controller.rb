875d01482bee7275587a7367ada7f4912e560a8c
jeffrey <johnsonjeff@gmail.com>
2015-09-04 16:59:32 -0700

    Refactor, push knowledge of path resolver to helper function

--- lib/progressive_render/controller.rb
+++ lib/progressive_render/controller.rb
@@ -5,20 +5,24 @@ module ProgressiveRender
     include Helpers
 
     def progressive_render(template=nil)
-      tc = Rails::PathResolver::TemplateContext.new
-
-      tc.type       = :controller
-      tc.controller = request.params["controller"]
-      tc.action     = request.params["action"]
-      pr = Rails::PathResolver.new(tc)
-
       if progressive_request.is_main_load?
-        render pr.path_for(template)
+        render resolve_path(template)
       else
-        content = render_to_string template: pr.path_for(template), layout: false
+        content = render_to_string template: resolve_path(template), layout: false
         stripped = Nokogiri::HTML(content).at_css("div##{progressive_request.fragment_name}_progressive_render")
         render text: stripped
       end
+    end
+
+    def resolve_path(template)
+      tc = Rails::PathResolver::TemplateContext.new
+      tc.type       = :controller
+      tc.controller = request.params["controller"]
+      tc.action     = request.params["action"]
+
+      pr = Rails::PathResolver.new(tc)
+
+      pr.path_for(template)
     end
   end
 end
