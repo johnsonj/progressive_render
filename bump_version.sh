6a172014f7ec7d6d2329755f566893ca16365a03
Jeff Johnson <johnsonjeff@gmail.com>
2016-07-31 15:44:19 -0700

    Allow specific version bumps

--- bump_version.sh
+++ bump_version.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-VERSION='patch'
+#
+# Bump the gem version
+# Pass in 'patch, minor, major' to specify what to update. Default is patch.
+#
+# This is needed to ensure all the Gemfile.locks are up to date with the
+# new version. Otherwise any CI builds will fail.
+#
 
 gem install gem-release
-gem bump --version $VERSION
+gem bump --version ${1:-patch}
 cd spec/dummy
 bundle install
 cd ../../
